e81f41146afef30b887d16597046b1fbc8b1c41e
ChristianBarsana <christian.barsana@linoflaptech.com>
2026-06-30 13:30:07 +0800

    Update project.ts

--- src/const/project.ts
+++ src/const/project.ts
@@ -13,7 +13,7 @@ export type ProjectItem = {
 export const projects: ProjectItem[] = [
   {
     title: "IM.AI Tablet Application",
-    image: "https://christianbarsana.netlify.app/uploads/im.ai.gif",
+    images: ["https://christianbarsana.netlify.app/uploads/im.ai.gif"],
     imageAlt: "IM.AI Tablet App",
     badges: ["Python", "PyQt", "SQL Server", "TensorFlow", "MongoDB"],
     description:
